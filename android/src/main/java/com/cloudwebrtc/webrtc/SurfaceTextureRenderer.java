c922fb301e89329608d169aebd98a8e6d83c7eb8
Internext IO <163850574+internextbarco@users.noreply.github.com>
2025-04-20 22:41:38 -0500

    Update SurfaceTextureRenderer.java

--- android/src/main/java/com/cloudwebrtc/webrtc/SurfaceTextureRenderer.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/SurfaceTextureRenderer.java
@@ -14,16 +14,7 @@ import java.util.concurrent.CountDownLatch;
 
 import io.flutter.view.TextureRegistry;
 
-/**
- * Display the video stream on a Surface.
- * renderFrame() is asynchronous to avoid blocking the calling thread.
- * This class is thread safe and handles access from potentially three different threads:
- * Interaction from the main app in init, release and setMirror.
- * Interaction from C++ rtc::VideoSinkInterface in renderFrame.
- * Interaction from SurfaceHolder lifecycle in surfaceCreated, surfaceChanged, and surfaceDestroyed.
- */
 public class SurfaceTextureRenderer extends EglRenderer {
-  // Callback for reporting renderer events. Read-only after initilization so no lock required.
   private RendererCommon.RendererEvents rendererEvents;
   private final Object layoutLock = new Object();
   private boolean isRenderingPaused;
@@ -32,9 +23,6 @@ public class SurfaceTextureRenderer extends EglRenderer {
   private int rotatedFrameHeight;
   private int frameRotation;
 
-  /**
-   * In order to render something, you must first call init().
-   */
   public SurfaceTextureRenderer(String name) {
     super(name);
   }
@@ -44,12 +32,6 @@ public class SurfaceTextureRenderer extends EglRenderer {
     init(sharedContext, rendererEvents, EglBase.CONFIG_PLAIN, new GlRectDrawer());
   }
 
-  /**
-   * Initialize this class, sharing resources with |sharedContext|. The custom |drawer| will be used
-   * for drawing frames on the EGLSurface. This class is responsible for calling release() on
-   * |drawer|. It is allowed to call init() to reinitialize the renderer after a previous
-   * init()/release() cycle.
-   */
   public void init(final EglBase.Context sharedContext,
                    RendererCommon.RendererEvents rendererEvents, final int[] configAttributes,
                    RendererCommon.GlDrawer drawer) {
@@ -63,17 +45,13 @@ public class SurfaceTextureRenderer extends EglRenderer {
     }
     super.init(sharedContext, configAttributes, drawer);
   }
+
   @Override
   public void init(final EglBase.Context sharedContext, final int[] configAttributes,
                    RendererCommon.GlDrawer drawer) {
     init(sharedContext, null /* rendererEvents */, configAttributes, drawer);
   }
-  /**
-   * Limit render framerate.
-   *
-   * @param fps Limit render framerate to this value, or use Float.POSITIVE_INFINITY to disable fps
-   *            reduction.
-   */
+
   @Override
   public void setFpsReduction(float fps) {
     synchronized (layoutLock) {
@@ -81,6 +59,7 @@ public class SurfaceTextureRenderer extends EglRenderer {
     }
     super.setFpsReduction(fps);
   }
+
   @Override
   public void disableFpsReduction() {
     synchronized (layoutLock) {
@@ -88,6 +67,7 @@ public class SurfaceTextureRenderer extends EglRenderer {
     }
     super.disableFpsReduction();
   }
+
   @Override
   public void pauseVideo() {
     synchronized (layoutLock) {
@@ -95,11 +75,11 @@ public class SurfaceTextureRenderer extends EglRenderer {
     }
     super.pauseVideo();
   }
-  // VideoSink interface.
+
   @Override
   public void onFrame(VideoFrame frame) {
-    if(surface == null) {
-      producer.setSize(frame.getRotatedWidth(),frame.getRotatedHeight());
+    if (surface == null) {
+      producer.setSize(frame.getRotatedWidth(), frame.getRotatedHeight());
       surface = producer.getSurface();
       createEglSurface(surface);
     }
@@ -108,24 +88,23 @@ public class SurfaceTextureRenderer extends EglRenderer {
   }
 
   private Surface surface = null;
-
   private TextureRegistry.SurfaceProducer producer;
 
   public void surfaceCreated(final TextureRegistry.SurfaceProducer producer) {
     ThreadUtils.checkIsOnMainThread();
     this.producer = producer;
     this.producer.setCallback(
-            new TextureRegistry.SurfaceProducer.Callback() {
-              @Override
-              public void onSurfaceAvailable() {
-                // Do surface initialization here, and draw the current frame.
-              }
+      new TextureRegistry.SurfaceProducer.Callback() {
+        @Override
+        public void onSurfaceAvailable() {
+          // Do surface initialization here, and draw the current frame.
+        }
 
-              @Override
-              public void onSurfaceDestroyed() {  // Changed from onSurfaceCleanup() to onSurfaceDestroyed()
-                surfaceDestroyed();
-              }
-            }
+        @Override
+        public void onSurfaceDestroyed() {
+          surfaceDestroyed();
+        }
+      }
     );
   }
 
@@ -137,7 +116,6 @@ public class SurfaceTextureRenderer extends EglRenderer {
     surface = null;
   }
 
-  // Update frame dimensions and report any changes to |rendererEvents|.
   private void updateFrameDimensionsAndReportEvents(VideoFrame frame) {
     synchronized (layoutLock) {
       if (isRenderingPaused) {
@@ -150,11 +128,11 @@ public class SurfaceTextureRenderer extends EglRenderer {
         }
       }
       if (rotatedFrameWidth != frame.getRotatedWidth()
-              || rotatedFrameHeight != frame.getRotatedHeight()
-              || frameRotation != frame.getRotation()) {
+          || rotatedFrameHeight != frame.getRotatedHeight()
+          || frameRotation != frame.getRotation()) {
         if (rendererEvents != null) {
           rendererEvents.onFrameResolutionChanged(
-                  frame.getBuffer().getWidth(), frame.getBuffer().getHeight(), frame.getRotation());
+            frame.getBuffer().getWidth(), frame.getBuffer().getHeight(), frame.getRotation());
         }
         rotatedFrameWidth = frame.getRotatedWidth();
         rotatedFrameHeight = frame.getRotatedHeight();
